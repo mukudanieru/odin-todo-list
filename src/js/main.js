28647a7832272000d71ca553bfc426e8828d4fcb
mukudanieru <111953203+mukudanieru@users.noreply.github.com>
2025-12-05 13:00:52 +0800

    Implement HTML structure and theme functionality - Integrated Basecoat UI into the project for a consistent design system - Added foundational HTML structure: header, main content area, and footer - Defined and applied a custom color scheme for light and dark modes. - Implemented a dynamic light/dark mode functionality using Tailwind CSS and JavaScript

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,12 @@
-console.log("Hello, World!");
+import "basecoat-css/all";
+
+const toggleBtn = document.querySelector("#theme-toggle");
+
+toggleBtn.addEventListener("click", () => {
+    console.log(localStorage);
+    document.body.classList.toggle("dark");
+    localStorage.setItem(
+        "theme",
+        document.body.classList.contains("dark") ? "dark" : "light"
+    );
+});
