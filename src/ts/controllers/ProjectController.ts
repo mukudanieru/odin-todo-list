5398de0e837fd8eecadb663e00eed64c7f53455d
mukudanieru <111953203+mukudanieru@users.noreply.github.com>
2025-12-19 12:56:05 +0800

    Add project form validation and dropdown menu UI - Added blank name validation with error messages - Cleared error state on user input and form cancel - Reset form state when closing dialog - Added dropdown menu with Edit and Delete options

--- src/ts/controllers/ProjectController.ts
+++ src/ts/controllers/ProjectController.ts
@@ -14,22 +14,60 @@ export default function ProjectController() {
       const projectFormSubmission = document.querySelector<HTMLFormElement>(
         "#project-form-submission"
       );
-
       if (!projectFormSubmission) return;
 
+      const nameInput = document.querySelector<HTMLInputElement>("#name");
+      if (!nameInput) return;
+
+      const cancelProjectForm = document.querySelector<HTMLButtonElement>(
+        "#cancel-create-project"
+      );
+      if (!cancelProjectForm) return;
+
+      const errorDiv = document.querySelector<HTMLDivElement>("#error-message");
+      if (!errorDiv) return;
+
+      // FORM SUBMISSION EVENT
       projectFormSubmission.addEventListener("submit", (e) => {
         e.preventDefault();
 
         const id = crypto.randomUUID();
-        const nameInput = document.querySelector<HTMLInputElement>("#name");
-        if (!nameInput) return;
 
         const name = nameInput.value.trim();
+
+        if (name === "") {
+          nameInput.classList.remove("border-muted", "dark:border-muted-dark");
+          nameInput.classList.add("border-red-500", "dark:border-red-400");
+          errorDiv.innerHTML = "Project name cannot be blank";
+          errorDiv.classList.remove("hidden");
+
+          return;
+        }
 
         projectRepository.add(Project({ id, name }));
         projectView.render(projectRepository.getAll());
 
         nameInput.value = "";
+        projectFormSubmission.closest("dialog")?.close();
+      });
+
+      // INPUT HANDLING SUBMISSION
+      nameInput.addEventListener("keydown", () => {
+        nameInput.classList.remove("border-red-500", "dark:border-red-400");
+        nameInput.classList.add("border-muted", "dark:border-muted-dark");
+        errorDiv.innerHTML = "";
+        errorDiv.classList.add("hidden");
+      });
+
+      // CANCEL PROJECT FORM EVENT
+      cancelProjectForm.addEventListener("click", () => {
+        cancelProjectForm.closest("dialog")?.close();
+        nameInput.value = "";
+
+        nameInput.classList.remove("border-red-500", "dark:border-red-400");
+        nameInput.classList.add("border-muted", "dark:border-muted-dark");
+        errorDiv.innerHTML = "";
+        errorDiv.classList.add("hidden");
       });
     },
   };
